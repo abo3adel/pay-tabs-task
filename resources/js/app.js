d2200ffde32782bde05cb1cf6a943dddbe5c6c1a
abo3adel <abo3adel35@gmail.com>
2023-01-05 19:49:52 +0000

    install choises

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,10 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+// const element = document.querySelector('#category_id');
+// import Choices from '../../node_modules/choices.js/public/assets/scripts/choices.min.js';
+// const choices = new Choices(element);
+
 window.Alpine = Alpine;
 
 Alpine.start();
