7091981b148fbfe949dd4739d2b1105fe60de2fc
abo3adel <abo3adel35@gmail.com>
2023-01-05 19:33:19 +0000

    install frontend assets

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,14 @@
 import { defineConfig } from 'vite';
-// import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        // https: {
-        //     key: "E:/laragon/etc/ssl/laragon.key",
-        //     cert: "E:/laragon/etc/ssl/laragon.crt",
-        // },
-        host: 'localhost',
-    },
     plugins: [
-        laravel(['resources/css/app.css', 'resources/js/app.ts']),
-        {
-            name: 'blade',
-            handleHotUpdate({ file, server }) {
-                if (file.endsWith('.blade.php')) {
-                    server.ws.send({
-                        type: 'full-reload',
-                        path: '*',
-                    });
-                }
-            },
-        },
-        // vue({
-        //     template: {
-        //         transformAssetUrls: {
-        //             base: null,
-        //             includeAbsolute: false,
-        //         },
-        //     },
-        // }),
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
     ],
 });
