05b4816a5e6c674943e75c3466c891653973ee3a
abo3adel <abo3adel35@gmail.com>
2023-01-05 20:33:04 +0000

    load sub categories for select category

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import $ from "jquery";
+
 // const element = document.querySelector('#category_id');
 // import Choices from '../../node_modules/choices.js/public/assets/scripts/choices.min.js';
 // const choices = new Choices(element);
@@ -9,3 +11,38 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+$(document).ready(function() {
+    let latestLevel = 1;
+    $(document).on('change', '.select', async function(ev) {
+        // alert('Alpine is ready');
+
+        const val = $(this).val();
+        
+        const res = await axios.get('sub-categories/' + val);
+
+        if (!res || !res.data) {
+            alert('error');
+            return;
+        }
+
+        if (!res.data.sub_categories.length) return;
+
+        const categories = res.data.sub_categories;
+        if (categories[0].level > latestLevel) {
+            latestLevel = categories[0].level;
+            // create new select menu for new data
+            let code = `
+                <div class="py-4">
+                    <select class="select bg-gray-700 text-white" name="category_id2" id="category_id2" >`;
+                        for (let category of categories) {
+                            code += `<option value="${category.id}">${category.name}</option>`;
+                        }
+                   code += `</select>
+                </div>
+            `;
+
+            $('#sub-categories').append(code);
+        }
+    });
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,13 @@
 <x-guest-layout>
     <div class="w-full text-center">
-        <select name="category_id" id="category_id" class="bg-gray-700 text-white">
+        <select  name="category_id" id="category_id1" class="select bg-gray-700 text-white">
             @foreach ($categories as $category)
                 <option value="{{ $category->id }}">{{ $category->name }}</option>
             @endforeach
         </select>
+
+        <div id="sub-categories">
+
+        </div>
     </div>
 </x-guest-layout>
