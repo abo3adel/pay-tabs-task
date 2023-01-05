1fa8274ba59e99aa82df14733b936bf824310858
abo3adel <abo3adel35@gmail.com>
2023-01-05 19:28:30 +0000

    seed db with 4 levels of categories

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -14,6 +16,34 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        $parentCategories = Category::factory(3)->sequence(
+            ['name' => 'Category A'],
+            ['name' => 'Category B'],
+            ['name' => 'Category C']
+        )->create([
+            'category_id' => null,
+        ]);
+
+        $this->createSubCategories($parentCategories, 9);
+
+        $this->createSubCategories(Category::whereLevel(2)->get(), 4, 3);
+
+        $this->createSubCategories(Category::whereLevel(3)->get(), 8, 4);
+    }
+
+    private function createSubCategories(Collection $parentCategories, int $subInx, int $level = 2): void
+    {
+        foreach ($parentCategories as $parent) {
+            $split = $level > 2 ? '-' : '';
+            Category::factory(3)->sequence(
+                ['name' => str_repeat('Sub ', $level - 1) . substr($parent->name, $subInx) . $split . 1],
+                ['name' => str_repeat('Sub ', $level - 1) . substr($parent->name, $subInx) . $split . 2],
+                ['name' => str_repeat('Sub ', $level - 1) . substr($parent->name, $subInx) . $split . 3],
+            )->create([
+                'category_id' => $parent->id,
+                'level' => $level
+            ]);
+        }
     }
 }
